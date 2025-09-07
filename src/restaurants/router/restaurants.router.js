8d926e8a3290b0000b9325b69a54108f9382cd12
김민호 <2022305011@skuniv.ac.kr>
2025-09-07 18:02:17 +0900

    🐛 Fix: restaurant detail endpoint 통합

--- src/restaurants/router/restaurants.router.js
+++ src/restaurants/router/restaurants.router.js
@@ -1,8 +1,7 @@
 import { Router } from "express";
 import {
   ensureRestaurantCtrl,
-  getRestaurantDetailCtrl,
-  getRestaurantExternalDetailCtrl,
+  getRestaurantFullDetailCtrl,
 } from "../controller/restaurants.controller.js";
 import { getNearbyRestaurantsCtrl } from "../controller/nearby.controller.js";
 
@@ -10,10 +9,10 @@ const r = Router();
 
 /** 네이버 기반 식당 검색 */
 r.get("/nearby", getNearbyRestaurantsCtrl);
-/** DB 상세조회 */
-r.get("/:restaurantId", getRestaurantDetailCtrl);
-/** 네이버 외부 상세 (메뉴/사진) */
-r.get("/:restaurantId/external", getRestaurantExternalDetailCtrl);
+
+/** DB + 네이버 통합 상세조회 */
+r.get("/:restaurantId/detail", getRestaurantFullDetailCtrl);
+
 /** 식당 확보(멱등) */
 r.put("/", ensureRestaurantCtrl);
 
